02fc4dbb09b69e7a763e9238505c4a3f210399ea
Christian Gärtner <chrisdagardner@gmail.com>
2020-12-01 10:01:38 +0100

    20201201 part two

--- 20201201.go
+++ 20201201.go
@@ -11,15 +11,47 @@ import (
 const _sumTarget = 2020
 
 func Solve20201201() error {
+	fmt.Println("---- 2020 12 01 ----")
+	defer func() {
+		fmt.Println("----    END     ----")
+	}()
+	err := Solve20201201PartOne()
+	if err != nil {
+		return err
+	}
+	return Solve20201201PartTwo()
+}
+
+func Solve20201201PartOne() error {
 	numbers, err := _read()
 	if err != nil {
 		return err
 	}
 
+	fmt.Println("Part One")
 	for i, a := range numbers {
 		for _, b := range numbers[i:] {
 			if a + b == _sumTarget {
 				fmt.Println(a * b)
+			}
+		}
+	}
+	return nil
+}
+
+func Solve20201201PartTwo() error {
+	numbers, err := _read()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Part Two")
+	for i, a := range numbers {
+		for j, b := range numbers[i:] {
+			for _, c := range numbers[j:] {
+				if a + b + c == _sumTarget {
+					fmt.Println(a * b * c)
+				}
 			}
 		}
 	}
