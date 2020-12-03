3e4530026c1b623da615df254f5d187210495872
Christian Gärtner <chrisdagardner@gmail.com>
2020-12-03 10:33:34 +0100

    rename packages to include digits instead of words

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/ChristianGaertner/aoc2020/common"
-	"github.com/ChristianGaertner/aoc2020/daythree"
+	"github.com/ChristianGaertner/aoc2020/day03"
 )
 
 func main() {
-	s := common.WithTiming(daythree.Solver{})
+	s := common.WithTiming(day03.Solver{})
 	if err := s.Solve(); err != nil {
 		panic(err)
 	}
