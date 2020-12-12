b366eea674651d7b54abf499610afb9427bd9c6f
Christian Gärtner <chrisdagardner@gmail.com>
2020-12-12 11:56:22 +0100

    20201212 part two

--- day12/20201212.go
+++ day12/20201212.go
@@ -13,18 +13,22 @@ type Command struct {
 	Amount int
 }
 
+func (c Command) String() string {
+	return fmt.Sprintf("%s%d", c.Action, c.Amount)
+}
+
 func (c Command) Vector() image.Point {
 	if c.Action == North {
-		return image.Pt(c.Amount, 0)
-	}
-	if c.Action == East {
 		return image.Pt(0, c.Amount)
 	}
+	if c.Action == East {
+		return image.Pt(c.Amount, 0)
+	}
 	if c.Action == South {
-		return image.Pt(-c.Amount, 0)
+		return image.Pt(0, -c.Amount)
 	}
 	if c.Action == West {
-		return image.Pt(0, -c.Amount)
+		return image.Pt(-c.Amount, 0)
 	}
 	return image.Pt(0, 0)
 }
@@ -134,10 +138,48 @@ func SolvePartOne() error {
 }
 
 func SolvePartTwo() error {
-	_, err := _read()
+	cmds, err := _read()
 	if err != nil {
 		return err
 	}
+
+	ship := image.Pt(0, 0)
+	wp := image.Pt(10, -1)
+	for _, cmd := range cmds {
+		switch cmd.Action {
+		case North:
+			wp.Y -= cmd.Amount
+		case South:
+			wp.Y += cmd.Amount
+		case East:
+			wp.X += cmd.Amount
+		case West:
+			wp.X -= cmd.Amount
+		case Left:
+			for i := 0; i < cmd.Amount; i += 90 {
+				wp.X, wp.Y = wp.Y, -wp.X
+			}
+		case Right:
+			for i := 0; i < cmd.Amount; i += 90 {
+				wp.X, wp.Y = -wp.Y, wp.X
+			}
+		case Forward:
+			ship.X += wp.X * cmd.Amount
+			ship.Y += wp.Y * cmd.Amount
+		}
+	}
+
+	x := ship.X
+	if x < 0 {
+		x = -x
+	}
+
+	y := ship.Y
+	if y < 0 {
+		y = -y
+	}
+
+	fmt.Println(x + y)
 
 	return nil
 }
